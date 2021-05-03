7bbe31a1c80fbbf1e6098f9a73ce61133de4f7c2
Michael Johnson <mcangelo200@gmail.com>
2021-05-03 00:15:32 +0000

    --worked on adding multiple componentes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
   <img alt="Vue logo" src="./assets/logo-3.png">
   <HelloWorld msg="School In-Site"/>
+  <!-- This is a sample learning staff -->
+  <br />
+  <h3>Button component</h3>
+  <ButtonClicked />
+  <button-clicked />
+  <!-- This is a counter component -->
+  <br>
+  <h3>Counter</h3>
+  <counter />
+  <event-handling></event-handling>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import ButtonClicked from '@/components/ButtonClicked';
+import Counter from '@/components/Counter';
+import EventHandling from '@/components/EventHandling';
+//import EventHandling from './components/EventHandling.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ButtonClicked,
+    Counter,
+    EventHandling,
   }
 }
 </script>
---
+++ src/components/ButtonClicked.vue
@@ -0,0 +1,16 @@
+<template>
+  <button @click="count++">
+    You clicked me {{ count }} times.
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonClicked',
+  data(){
+    return {
+        count: 0
+    }
+  }
+}
+</script>
